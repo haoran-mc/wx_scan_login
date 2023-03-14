3c7e319793037539437a8c333c1f72f510d947b8
haoran <haoran.mc@outlook.com>
2023-03-15 02:15:37 +0800

    use mysql instead of postgres

--- web-back-end/pkg/db/mysql.go
+++ web-back-end/pkg/db/mysql.go
@@ -1,1 +1,40 @@
 package db
+
+import (
+	"fmt"
+
+	"github.com/haoran-mc/wx_scan_login/web-back-end/internal/model"
+	"github.com/haoran-mc/wx_scan_login/web-back-end/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var gormDB *gorm.DB
+
+func DB() *gorm.DB {
+	return gormDB
+}
+
+func syncDB() {
+	_ = gormDB.AutoMigrate(
+		&model.Users{},
+	)
+}
+
+func init() {
+	mysqlDsn := fmt.Sprintf(
+		config.Conf.Mysql.Dsn,
+		config.Conf.Mysql.User,
+		config.Conf.Mysql.Pass,
+		config.Conf.Mysql.Dbname,
+	)
+
+	gormConfig := &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger:                                   logger.Default.LogMode(logger.Silent),
+	}
+
+	gormDB, _ = gorm.Open(postgres.Open(mysqlDsn), gormConfig)
+	syncDB()
+}
